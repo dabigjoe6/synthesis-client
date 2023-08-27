3bb2c3a577dc6e4b89ff3fd4e266e215dc07ad16
Joseph Olabisi <dabigjoe6@gmail.com>
2023-08-27 16:43:15 +0100

    try

--- src/pages/auth/components/index.ts
+++ src/pages/auth/components/index.ts
@@ -1,6 +1,6 @@
-export { default as Container } from "./container";
 export { default as Form } from "./form";
 export { default as H2 } from "./h2";
 export { default as H4 } from "./h4";
 export { default as GoogleSignIn } from './google-signin';
 export { default as EmailSignIn } from './email-signin';
+export { default as Container } from "./container";
