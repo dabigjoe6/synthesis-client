3281b7fa1f9688a2b69a6a6ea52398e44e680370
Joseph Olabisi <dabigjoe6@gmail.com>
2023-08-27 16:52:50 +0100

    Try

--- src/pages/auth/components/index.ts
+++ src/pages/auth/components/index.ts
@@ -1,4 +1,4 @@
-export { default as Form } from "./form";
+export { default as Form } from "./form.js";
 export { default as H2 } from "./h2";
 export { default as H4 } from "./h4";
 export { default as GoogleSignIn } from './google-signin';
